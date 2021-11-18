5bb504dd69b227bbd20c85ba3312b39375d72266
Alexandra-Konvisar <alexkonv95@gmail.com>
2021-11-18 16:40:13 +0300

    git init

--- tilda-products-1.0.js
+++ tilda-products-1.0.js
@@ -1,136 +1,183 @@
-function t_prod__init(recid){
-	var rec=$('#rec'+recid);
-	rec.find('.js-product').each(function() {
-		if($(this).find('.js-product-option').length){
-			t_prod__initProduct(recid,$(this));
+// eslint-disable-next-line
+function t_prod__init(recId) {
+	var rec = document.querySelector('#rec' + recId);
+	if (!rec) return;
+
+	var productElements = rec.querySelectorAll('.js-product');
+
+	Array.prototype.forEach.call(productElements, function(productElement) {
+		if (productElement.querySelector('.js-product-option')) {
+			t_prod__initProduct(recId, productElement);
 		}
 	});
 }
 
-function t_prod__initProduct(recid,el_prod){
-	t_prod__initPrice(recid,el_prod);
-	t_prod__addEvents__options(recid,el_prod);
-	t_prod__updatePrice(recid,el_prod);
+function t_prod__initProduct(recId, productElement) {
+	t_prod__initPrice(productElement);
+	t_prod__addEvents__options(recId, productElement);
+	t_prod__updatePrice(recId, productElement);
 }
 
-function t_prod__initPrice (recid,el_prod) {
-	var el_price=el_prod.find('.js-product-price');
-	if (el_price.length) {
-		/* */
+function t_prod__initPrice(productElement) {
+	// todo: jqeury in js
+	if (productElement[0] === undefined) {
+		var productElement = productElement;
 	} else {
-		el_prod.append("<div style='display:none;' class='js-product-price'></div>");
-		var el_price=el_prod.find('.js-product-price');
+		var productElement = productElement[0];
 	}
-	var price=el_price.html();
-	if (typeof price!='undefined' && price!='') {
-		if (! el_price.attr('data-product-price-def') && ! el_price.attr('data-product-price-def-str')) {
-			var p=t_prod__cleanPrice(price);
-			el_price.attr('data-product-price-def',p);
-			price = price.replace(/"/g,'');
-			el_price.attr('data-product-price-def-str',price);
+	// -->
+
+	var productPrice = productElement.querySelector('.js-product-price');
+
+	if (!productPrice) {
+		var wrapPrice = productElement.querySelector('.t-store__card__price');
+		wrapPrice.insertAdjacentHTML('beforeend', '<div style="display:none;" class="js-product-price"></div>');
+		productPrice = productElement.querySelector('.js-product-price');
+	}
+
+	var priceValue = productPrice.textContent;
+	var productDef = productPrice.getAttribute('data-product-price-def');
+	var productDefStr = productPrice.getAttribute('data-product-price-def-str');
+
+	if (priceValue) {
+		if (!productDef && !productDefStr) {
+			var price = t_prod__cleanPrice(priceValue);
+			productPrice.setAttribute('data-product-price-def', price);
+			productPrice.setAttribute('data-product-price-def-str', priceValue.replace(/"/g,''));
 		}
 	} else {
-		el_price.attr('data-product-price-def','0');
+		productPrice.setAttribute('data-product-price-def', 0);
 	}
 }
 
-function t_prod__updatePrice(recid, el_prod) {
-    var totalprice = 0;
+// Update the price when changing
+function t_prod__updatePrice(recId, productElement) {
+	// todo: jqeury in js
+	if (productElement[0] === undefined) {
+		var productElement = productElement;
+	} else {
+		var productElement = productElement[0];
+	}
+	// -->
+
+	var productPrice = productElement.querySelector('.js-product-price:not(.js-store-prod-price-range-val)');
+	var productSelects = productElement.querySelectorAll('.js-product-option-variants');
+    var totalPrice = 0;
     var add = 0;
-    var el_price = el_prod.find('.js-product-price:not(.js-store-prod-price-range-val)');
-		// If user selected to draw prices as price range via options, do not change price here
-		// Change price range in tilda-catalog only
-		if (!el_price.length) {
-			return;
+
+	if (!productPrice) return;
+
+	var priceValue = productPrice.getAttribute('data-product-price-def');
+
+	priceValue = priceValue * 1;
+
+	Array.prototype.forEach.call(productSelects, function(select) {
+		if (!select) return;
+		var optionPrice = select.querySelector('option:checked').getAttribute('data-product-variant-price');
+
+		if (optionPrice) {
+			var price = t_prod__cleanPrice(optionPrice);
+
+			if (optionPrice.indexOf('+') !== -1) {
+				add += price;
+			} else if (parseFloat(price) !== 0) {
+				priceValue = price;
+			}
 		}
-    var price = el_price.attr('data-product-price-def');
-    price = price * 1;
+	});
 
-    el_prod.find('.js-product-option-variants').each(function () {
-        var el_opt = $(this);
-        var op_price = el_opt.find('option:selected').attr('data-product-variant-price');
-        if (typeof op_price != 'undefined' && op_price != '') {
-            var p = t_prod__cleanPrice(op_price);
-            if (op_price.indexOf('+') > -1) {
-                add += p;
-            } else if (parseFloat(p) !== 0) {
-                price = p;
-            }
-        }
-    });
+	totalPrice = priceValue + add;
 
-    totalprice += price + add;
-
-    if (totalprice > 0) {
-        el_price.html(t_prod__showPrice(t_prod__roundPrice(totalprice)));
-    } else {
-        var defval = el_price.attr('data-product-price-def-str');
-        el_price.html(defval);
-    }
+	if (totalPrice > 0) {
+		productPrice.innerHTML = t_prod__showPrice(t_prod__roundPrice(totalPrice));
+	} else {
+		productPrice.innerHTML = productPrice.getAttribute('data-product-price-def-str');
+	}
 }
 
-function t_prod__addEvents__options(recid,el_prod){
-	var el=el_prod.find('.js-product-option-variants');
-	if(el.length){
-		el.change(function() {
-			var el_prod=$(this).closest('.js-product');
-			t_prod__updatePrice(recid,el_prod);
+// Add events for select
+function t_prod__addEvents__options(recId, productElement) {
+	// todo: jqeury in js
+	if (productElement[0] === undefined) {
+		var productElement = productElement;
+	} else {
+		var productElement = productElement[0];
+	}
+	// -->
+	var productSelects = productElement.querySelectorAll('.js-product-option-variants');
+
+	Array.prototype.forEach.call(productSelects, function(select) {
+		var product = t_getParent(select, 'js-product');
+		select.addEventListener('change', function() {
+			t_prod__updatePrice(recId, product);
 		});
-	}
+	});
 }
 
-function t_prod__cleanPrice(price){
-	if(typeof price=='undefined' || price === null || price == '' || price == 0){
-		price=0;
-	}else{
+// Clear price from symbols
+function t_prod__cleanPrice(price) {
+	if (price) {
 		price = price.toString();
-		price = price.replace(',','.');
-		price = price.replace(/[^0-9.]/g,'');
+		price = price.replace(',', '.').replace(/[^0-9.]/g, '');
 		price = parseFloat(price).toFixed(2);
-		if(isNaN(price))price=0;
+		if (isNaN(price)) price = 0;
 		price = parseFloat(price);
-		price = price*1;
-		if(price<0)price=0;
+		price = price * 1;
+		if (price < 0) price = 0;
+	} else {
+		price = 0;
 	}
 	return price;
 }
 
-function t_prod__roundPrice(price){
-	if(typeof price=='undefined' || price=='' || price==0){
-		price=0;
-	}else{
+function t_prod__roundPrice(price) {
+	if (price) {
 		price = parseFloat(price).toFixed(2);
 		price = parseFloat(price);
-		price = price*1;
-		if(price<0)price=0;
+		price = price * 1;
+		if (price < 0) price = 0;
+	} else {
+		price = 0;
 	}
 	return price;
 }
 
-function t_prod__showPrice(price){
-	if(typeof price=='undefined' || price==0 || price==''){
-		price='';
-	}else{
+// Transform price in string price
+function t_prod__showPrice(price) {
+	if (!price) {
+		price = '';
+	} else {
 		price = price.toString();
 
-		if(typeof window.tcart!='undefined' && typeof window.tcart['currency_dec']!='undefined' && window.tcart['currency_dec']=='00'){
-			if(price.indexOf('.')===-1 && price.indexOf(',')===-1){
-				price=price+'.00';
-			}else{
-				var foo=price.substr( price.indexOf('.')+1 );
-				if(foo.length===1){
-					price=price+'0';
-				}
+		if (window.tcart && window.tcart['currency_dec'] && window.tcart['currency_dec'] === '00') {
+			if (price.indexOf('.') === -1 && price.indexOf(',') === -1) {
+				price = price + '.00';
+			} else {
+				var foo = price.substr(price.indexOf('.') + 1);
+
+				if (foo.length === 1) price = price + '0';
 			}
 		}
 
 		price = price.replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 
-		if(typeof window.tcart!='undefined' && typeof window.tcart['currency_sep']!='undefined' && window.tcart['currency_sep']=='.'){
-			price = price.replace(',','.');
-		}else{
-			price = price.replace('.',',');
+		if (window.tcart && window.tcart['currency_sep'] && window.tcart['currency_sep'] === '.') {
+			price = price.replace(',', '.');
+		} else {
+			price = price.replace('.', ',');
 		}
 	}
 	return price;
+}
+
+// Method closest for ie
+function t_getParent(element, className) {
+	var node = element.parentNode;
+	while (node) {
+		if (node.classList.contains(className)) {
+			return node;
+		}
+		node = node.parentNode;
+	}
+	return undefined;
 }
