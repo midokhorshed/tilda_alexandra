b2585cd478d04ee44de94a1d4d10d072f380b9b4
Alexandra-Konvisar <alexkonv95@gmail.com>
2021-11-18 18:43:11 +0300

    git-init

--- tilda-hover-zoom-1.0.js
+++ tilda-hover-zoom-1.0.js
@@ -1,111 +1,128 @@
+// eslint-disable-next-line no-unused-vars
 function t_hoverZoom_init(recid) {
-    var rec = $('#rec' + recid);
-    var div = rec.find('.t-slds__container .t-slds__bgimg');
+    var parentHZContainer = document.getElementById('rec' + recid);
+    if (!parentHZContainer) return false;
+    var hoverZoomContainers = parentHZContainer.querySelectorAll('.t-slds__bgimg');
+    if (!hoverZoomContainers.length) return false;
 
-    div.each(function () {
-        var parent = $(this).parent();
-        var image = new Image();
-        image.classList.add('js-image-zoom-hover');
-        var url = $(this).attr('data-original');
-
-        /* if dont have image - return */
-        if (typeof url != 'undefined') {
-            if (url.indexOf('/-/empty/') >= 0 || url == '') {
-                return;
-            }
-        } else {
-            return;
-        }
-
-        /* set hidden, for zoomImage with pos absolute doesn't move from parent */
-        parent.css({
-            overflow: 'hidden'
-        });
-
-        t_hoverZoom_addScaledEl(recid, parent, image, url);
+    hoverZoomContainers.forEach(function (imgBlock) {
+        var parent = imgBlock.parentNode;
+        var img = document.createElement('img');
+        img.classList.add('js-image-zoom-hover');
+        var url = imgBlock.getAttribute('data-original');
+        // if hasn't img - return false
+        if (!url) return false;
+        if (~url.indexOf('/-/empty/') || url === '') return false;
+        //set overflow hidden for parent with pos absolute (doesn't move from parent)
+        parent.style.overflow = 'hidden';
+        t_hoverZoom_addScaledEl(recid, parent, img, url, parentHZContainer);
     });
-
-    $('head').append('<style>.t-slds__imgwrapper {position: relative;} .t-slds__item_active.t-slds__item .js-image-zoom-hover {display:block;} .t-slds__item .js-image-zoom-hover {display:none;}</style>');
+    document.head.insertAdjacentHTML('beforeend', '<style>.t-slds__imgwrapper {position: relative;} .t-slds__item_active.t-slds__item .js-image-zoom-hover {display:block;} .t-slds__item .js-image-zoom-hover {display:none;}</style>');
 }
 
-function t_hoverZoom_addScaledEl(recid, parent, image, url) {
-    var rec = $('#rec' + recid);
-
-    var zoomImage = $(image);
-    zoomImage.load(function () {
-        var parentForOffset = rec.find('.t-slds__item_active');
-        var parentWidth = parentForOffset.outerWidth();
-        var parentHeight = parentForOffset.outerHeight();
+function t_hoverZoom_addScaledEl(recid, parent, img, url, parentHZContainer) {
+    img.onload = function () {
+        var parentForOffset = parentHZContainer.querySelector('.t-slds__item_active');
+        var parentWidth = parentForOffset.offsetWidth;
+        var parentHeight = parentForOffset.offsetHeight;
         var xRatio;
         var yRatio;
         var timer;
         var delay = 0;
         var fade = 200;
-        var offset = parentForOffset.offset();
+        var offset = t_hover_zoom__getCoords(parentForOffset);
         var zoomEnable = false;
+        var zoomEnambleContainers = document.querySelectorAll('#rec' + recid + '-zoomEnable');
 
-        /* add image only if there is no image inside  */
-        if (parent.find('img')[0]) {
-            return;
+        //if parent has img inside - don't add img
+        if (parent.querySelector('img')) {
+            return false;
         }
-
+        //"this" in this context - img
         if (parentWidth > this.width || parentHeight > this.height) {
-            return;
-        } else {
-            $(this)
-                .css({
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    opacity: 0,
-                    width: image.width,
-                    height: image.height,
-                    maxWidth: 'none'
-                })
-                .mouseenter(function (el) {
-                    parentForOffset = $(this).parent();
-                    offset = parentForOffset.offset();
-                    var _this = $(this);
-                    timer = setTimeout(function () {
-                        parentWidth = parentForOffset.outerWidth();
-                        parentHeight = parentForOffset.outerHeight();
-
-                        xRatio = -(image.width - parentWidth) / parentWidth;
-                        yRatio = -(image.height - parentHeight) / parentHeight;
-                        _this
-                            .css('left', (el.pageX - offset.left) * xRatio)
-                            .css('top', (el.pageY - offset.top) * yRatio);
-                        _this.stop().fadeTo(fade, 1);
-                    }, delay);
-                })
-                .mousemove(function (el) {
-                    offset = parentForOffset.offset();
-                    $(this)
-                        .css('left', (el.pageX - offset.left) * xRatio)
-                        .css('top', (el.pageY - offset.top) * yRatio);
-                })
-                .mouseleave(function () {
-                    clearTimeout(timer);
-                    $(this)
-                        .stop()
-                        .fadeTo(fade, 0);
-                    var _this = $(this);
-                    setTimeout(function () {
-                        _this.css('left', 0).css('top', 0);
-                    }, 200);
-                });
-            $(this).appendTo(parent);
-
-            zoomEnable = true;
-
-            if (zoomEnable && $('#rec' + recid + '-zoomEnable').length < 1) {
-                $("<style id='rec" + recid + "-zoomEnable'>" +
-                    '#rec' + recid + ' .t-slds__arrow_wrapper { pointer-events: none }' +
-                    '#rec' + recid + ' .t-slds__arrow { pointer-events: all }' +
-                    '</style>').appendTo(parent);
-            }
+            return false;
         }
-    });
+        this.style.cssText = 'position: absolute; top: 0; left: 0; opacity: 0; width: ' + this.width + 'px; height: ' + this.height + 'px; max-width: none;';
 
-    zoomImage.attr('src', url);
+        this.addEventListener('mouseenter', function (e) {
+            //if screen width changed
+            parentForOffset = img.parentNode;
+            offset = t_hover_zoom__getCoords(parentForOffset);
+            timer = setTimeout(function () {
+                parentWidth = parentForOffset.offsetWidth;
+                parentHeight = parentForOffset.offsetHeight;
+
+                xRatio = (parentWidth - img.width) / parentWidth;
+                yRatio = (parentHeight - img.height) / parentHeight;
+
+                //"this" in this context - NOT img, use "img"
+                img.style.left = ((e.pageX - offset.left) * xRatio) + 'px';
+                img.style.top = ((e.pageY - offset.top) * yRatio) + 'px';
+            }, delay);
+        });
+
+        this.addEventListener('mousemove', function (e) {
+            offset = t_hover_zoom__getCoords(parentForOffset);
+            img.style.left = ((e.pageX - offset.left) * xRatio) + 'px';
+            img.style.top = ((e.pageY - offset.top) * yRatio) + 'px';
+        });
+
+        this.addEventListener('mouseleave', function () {
+            clearTimeout(timer);
+        });
+
+        t_hover_zoom_hoverFade(img, fade);
+
+        parent.append(this);
+
+        zoomEnable = true;
+
+        if (zoomEnable && zoomEnambleContainers.length > 1) {
+            parent.insertAdjacentHTML('beforeend', '<style id="rec' + recid + '-zoomEnable">' + '#rec' + recid + ' .t-slds__arrow_wrapper { pointer-events: none }' + '#rec' + recid + ' .t-slds__arrow { pointer-events: all }' + '</style>');
+        }
+
+    };
+
+    img.setAttribute('src', url);
+
+    function t_hover_zoom_hoverFade(el, duration) {
+        if (!el) return false;
+        //timerFadeIn need to clear interval, when user leaves this el area
+        var timerFadeIn;
+        el.addEventListener('mouseenter', function () {
+            var opacityPercent = +(el.style.opacity) * 100;
+            //fadeIn when mouse enters the el area
+            timerFadeIn = setInterval(function () {
+                if (opacityPercent < 100) {
+                    ++opacityPercent;
+                    el.style.opacity = opacityPercent / 100;
+                } else {
+                    clearInterval(timerFadeIn);
+                }
+            }, (duration / 200));
+        });
+        el.addEventListener('mouseleave', function () {
+            clearInterval(timerFadeIn);
+            var opacityPercent = +(el.style.opacity) * 100;
+            //fadeOut func
+            var timerFadeOut = setInterval(function () {
+                if (opacityPercent > 0) {
+                    --opacityPercent;
+                    el.style.opacity = opacityPercent / 100;
+                } else {
+                    clearInterval(timerFadeOut);
+                }
+            }, (duration / 200));
+        });
+    }
+
+    //analog of the $.offset();
+    function t_hover_zoom__getCoords(element) {
+        if (element) {
+            var rect = element.getBoundingClientRect();
+            var top = rect.top + window.pageYOffset;
+            var left = rect.left + window.pageXOffset;
+            return { top: top, left: left };
+        }
+    }
 }
